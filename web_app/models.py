a5113a47472e85aefd5b7cc50a4c3b19b165c385
unknown <s3975162@rmit.edu.vn>
2023-11-09 00:27:47 +0700

    add models

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    ACCOUNT_TYPES = [
+        ("V", "Vendor"),
+        ("C", "Customer")
+    ]
+    username = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=255)
+    avatar = models.CharField(max_length=255, blank=True)
+    account_type = models.CharField(
+        max_length=1, choices=ACCOUNT_TYPES)
+
+
+class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    images = []
+    description = models.TextField(blank=True)
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = [{
+        'product': models.ManyToManyField(Product),
+        'quantity': models.IntegerField()
+    }]
+    total_price = models.IntegerField()
+
+
+class Order(models.Model):
+    ORDER_STATUSES = [
+        ("A", "Active"),
+        ("D", "Delivered"),
+        ("C", "Canceled")
+    ]
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    products = [{
+        'product': models.ManyToManyField(Product),
+        'quantity': models.IntegerField()
+    }]
+    total_price = models.IntegerField()
+    status = []
