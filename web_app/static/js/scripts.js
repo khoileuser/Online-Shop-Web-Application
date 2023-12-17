051e7c3f261536af652295f0afe0382ac4f5d7e7
unknown <s3975162@rmit.edu.vn>
2023-12-17 18:03:12 +0700

    add landing hero and remake nav

--- web_app/static/js/scripts.js
+++ web_app/static/js/scripts.js
@@ -1,14 +1,3 @@
-// Sets the source of a frame element to the URL of the selected file.
-function preview() {
-  frame.src = URL.createObjectURL(event.target.files[0]);
-}
-
-// Clears the value of a file input element and sets the source of an image element to a default image.
-function clearImage() {
-  document.getElementById('formFile').value = "";
-  frame.src = "/images/profiles/default.jpg";
-}
-
 function customAlert(message) {
   var alertContainer = document.querySelector('.alert-container');
   var alertMsg = document.querySelector('.alert-msg');
