931f689c3dd736bed3ac177fcfa364e2bc040d3d
unknown <s3975162@rmit.edu.vn>
2024-01-15 23:32:53 +0700

    add views and wishlist tests

--- web_app/tests/views.py
+++ web_app/tests/views.py
@@ -1,2 +1,26 @@
 from django.test import TestCase
-from web_app.models import *
+
+
+class CommonViewsTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_index_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_view(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_terms_view(self):
+        response = self.client.get('/terms/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_privacy_view(self):
+        response = self.client.get('/privacy/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_execute_view(self):
+        response = self.client.get('/execute/')
+        self.assertEqual(response.status_code, 200)
